4ea5414df26e6fbb31294564026f667229d51b38
Julien M'Poy <julien.mpoy@gmail.com>
2017-05-25 12:31:07 +0200

    First project structure

---
+++ Ouiches/index.android.js
@@ -0,0 +1,63 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  Button,
+  Alert,
+  View
+} from 'react-native';
+
+const onButtonPress = () => { Alert.alert('Button has been pressed!'); };
+
+export default class Ouiches extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native! Ow yeah!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
+        </Text>
+        <Button
+          title="Press Me"
+          onPress={onButtonPress}
+          color="#841585"
+          accessibilityLabel="No more info"
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('Ouiches', () => Ouiches);
---
+++ README.md
@@ -0,0 +1,6 @@
+# Ouiches
+
+A simple sound box for Android allowing you to enjoy the best quotes
+of the movie «La Classe Américaine». The application is developed for Android
+using Facebook's [React Native](https://facebook.github.io/react-native/)
+framework.
